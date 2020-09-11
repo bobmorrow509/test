ffc14f5102da8c8531749e6d93271cde34ac80e0
bob morrow <bobmorrow509@outlook.com>
2020-09-11 17:59:02 +1000

    This is the first commit

---
+++ client/src/components/Demo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Demo extends Component {
+
+  render() {
+
+    return(
+      <div>
+        <h1>This is the Demo Heading</h1>
+      </div>
+    )
+  }
+}
+
+export default Demo
---
+++ client/src/components/Topic/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Topic extends Component {
+
+  render() {
+
+    return(
+      <div>
+        <h1>Topic</h1>
+      </div>
+    )
+  }
+}
+
+export default Topic
